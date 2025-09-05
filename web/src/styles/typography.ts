bed20a1551d05fa113a838f1212017c861dea51a
eunchae-04 <rhdmsco1163@naver.com>
2025-09-06 03:07:14 +0900

    style(typography): vh 단위로 타이포그래피 크기 및 줄 높이 조정

--- web/src/styles/typography.ts
+++ web/src/styles/typography.ts
@@ -2,63 +2,68 @@
 export const typography = {
   // 버튼 (Button)
   button1Bold: {
-    fontSize: '2.1rem', // "안내 시작" 버튼
+    fontSize: '3.76vh', // "안내 시작" 버튼
     fontWeight: 700,
-    lineHeight: '2.5rem',
+    lineHeight: '4.7vh',
   },
   button2Bold: {
-    fontSize: '2.3rem', // "가까운 쉼터 찾기" 버튼
+    fontSize: '4.33vh', // "가까운 쉼터 찾기" 버튼
     fontWeight: 700,
-    lineHeight: '3.2rem',
+    lineHeight: '6.03vh',
   },
   button3Bold: {
-    fontSize: '1.9rem', // "내 위치" 버튼, "안내 시작" 버튼
+    fontSize: '3.57vh', // "내 위치" 버튼, "안내 시작" 버튼
     fontWeight: 700,
-    lineHeight: '1.7rem',
+    lineHeight: '3.2vh',
   },
 
   body1Regular: {
-    fontSize: '2rem', // 권한 거부 안내 문구
+    fontSize: '3.76vh', // 권한 거부 안내 문구
     fontWeight: 800,
-    lineHeight: '2.8rem',
+    lineHeight: '5.27vh',
   },
   body2Bold: {
-    fontSize: '1.4rem', // 카드 내 거리, 운영시간
+    fontSize: '2.63vh', // 카드 내 거리, 운영시간
     fontWeight: 700,
-    lineHeight: '1.8rem',
+    lineHeight: '3.39vh',
   },
   body3Bold: {
-    fontSize: '1.2rem', // 카드 내 거리, 운영시간
+    fontSize: '2.26vh', // 카드 내 거리, 운영시간
     fontWeight: 600,
-    lineHeight: '1.5rem',
+    lineHeight: '2.64vh',
   },
 
   // 제목 (Title)
   title1Bold: {
-    fontSize: '1.8rem', // 쉼터 이름, "더보기" 버튼
+    fontSize: '3.38vh', // 쉼터 이름, "더보기" 버튼
     fontWeight: 700,
-    lineHeight: '2rem',
+    lineHeight: '3.52vh',
   },
   title2Bold: {
-    fontSize: '1.8rem', // 쉼터 이름
+    fontSize: '3.38vh', // 쉼터 이름
     fontWeight: 800,
-    lineHeight: '1.9rem',
+    lineHeight: '3.34vh',
   },
 
   // 기타 강조 텍스트
   highlight1Bold: {
-    fontSize: '1.2rem', // 평점 숫자
+    fontSize: '2.26vh', // 평점 숫자
     fontWeight: 700,
-    lineHeight: '1.7rem',
+    lineHeight: '3.2vh',
   },
   highlight2Bold: {
-    fontSize: '1.3rem', // 평정 숫자
+    fontSize: '2.44vh', // 평정 숫자
     fontWeight: 700,
-    lineHeight: '1.6rem',
+    lineHeight: '3.02vh',
   },
   highlight3Bold: {
-    fontSize: '1.1rem', // 운영중, 야외 태그
+    fontSize: '2.07vh', // 운영중, 야외 태그
     fontWeight: 700,
-    lineHeight: '1.6rem',
+    lineHeight: '3.02vh',
   },
 } as const;
+
+/*
+1rem = 16px
+vh 값 = (rem 값 * 16 / 851) * 100
+*/
